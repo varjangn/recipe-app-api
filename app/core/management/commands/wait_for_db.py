352e63502f0889fc72b0e37c44a6ff0433108b5f
varjang <varjangnandaniya8@gmail.com>
2022-12-14 14:23:40 +0530

    added postgres db server & wait_for_db command

---
+++ app/core/management/commands/wait_for_db.py
@@ -0,0 +1,27 @@
+"""
+Django command to wait for the database to be available
+"""
+
+import time
+
+from psycopg2 import OperationalError as Psycopg2OpError
+
+from django.core.management.base import BaseCommand
+from django.db.utils import OperationalError
+
+
+class Command(BaseCommand):
+    """Django Command to wait for database connection"""
+
+    def handle(self, *args, **options):
+        self.stdout.write("Waiting for database...")
+        db_up = False
+        while not db_up:
+            try:
+                self.check(databases=['default'])
+                db_up = True
+            except (Psycopg2OpError, OperationalError):
+                self.stdout.write('Database unavailable, retrying in 1 sec...')
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database available..."))
