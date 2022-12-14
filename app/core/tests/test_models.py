2ca41843eb6f35f6209b45fcf65523b542179751
varjang <varjangnandaniya8@gmail.com>
2022-12-14 15:39:05 +0530

    fixed test case

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -10,11 +10,11 @@ class ModelTests(TestCase):
 
     def test_create_user_with_email_successful(self):
         """Test creating a user with an email."""
-        email = "test@example.com"
-        password = "testpass123"
-        user = get_user_model().objects.create(
+        email = 'test@example.com'
+        password = 'testpass123'
+        user = get_user_model().objects.create_user(
             email=email,
-            password=password
+            password=password,
         )
 
         self.assertEqual(user.email, email)
