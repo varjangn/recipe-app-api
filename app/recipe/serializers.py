c3ad39c9d293fe11092d6961e632abb3f1533325
varjang <varjangnandaniya8@gmail.com>
2022-12-19 13:19:45 +0530

    update or clear tags in recipe update API

--- app/recipe/serializers.py
+++ app/recipe/serializers.py
@@ -23,11 +23,8 @@ class RecipeSerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'time_minutes', 'price', 'link', 'tags']
         read_only_fields = ['id']
 
-    def create(self, validated_data):
-        """Create recipe with tags"""
-        tags = validated_data.pop('tags', [])
-        recipe = models.Recipe.objects.create(**validated_data)
-
+    def _get_or_create_tags(self, tags, recipe):
+        """Handle getting or creating tags as needed"""
         auth_user = self.context['request'].user
         for tag in tags:
             tag_obj, created = models.Tag.objects.get_or_create(
@@ -36,7 +33,27 @@ class RecipeSerializer(serializers.ModelSerializer):
             )
             recipe.tags.add(tag_obj)
 
+    def create(self, validated_data):
+        """Create recipe with tags"""
+        tags = validated_data.pop('tags', [])
+        recipe = models.Recipe.objects.create(**validated_data)
+        self._get_or_create_tags(tags, recipe)
+
         return recipe
+
+    def update(self, instance, validated_data):
+        """Update recipe."""
+        tags = validated_data.pop('tags', None)
+        if tags is not None:
+            instance.tags.clear()
+            self._get_or_create_tags(tags, instance)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
+
 
 class RecipeDetailSerializer(RecipeSerializer):
     """Serializer for single recipe instance"""
