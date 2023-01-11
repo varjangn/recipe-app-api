2c70130dfb1fcc85adba25869bfe3ca8db284722
varjang <varjangnandaniya8@gmail.com>
2023-01-11 10:45:08 +0530

    fixed linting errors

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -88,7 +88,6 @@ class RecipeViewSet(viewsets.ModelViewSet):
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
-
 @extend_schema_view(
     list=extend_schema(
         parameters=[
